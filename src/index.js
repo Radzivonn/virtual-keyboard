12f0f4fa15e1f41d6580e0373fc75723220ea8bd
Rodion <rodionserez@gmail.com>
2023-05-01 12:00:52 +0300

    feat: add layout and keyboard generation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './style/style.scss';
+import ENkeys from './modules/ENkeys.json';
+import RUkeys from './modules/RUkeys.json';
+import { getDefaultLayout } from './modules/getDefaultLayout';
+import { getKeyboardLayout } from './modules/getKeyboardLayout';
+import Keyboard from './modules/keyboardClass';
+
+const body = document.querySelector('body');
+
+// 
+body.insertAdjacentHTML('afterbegin', getDefaultLayout());
+const wrapper = document.querySelector('.wrapper');
+wrapper.insertAdjacentHTML('beforeend', getKeyboardLayout(ENkeys, RUkeys));
+wrapper.insertAdjacentHTML('beforeend', '<p> Клавиатура создана в операционной системе Windows <br> Для переключения языка комбинация: левыe ctrl + alt</p>');
+
+const keyboardObject = new Keyboard(document.querySelector('.keyboard'));
+
+console.log(keyboardObject);
+
+keyboardObject.addMouseListeners();
