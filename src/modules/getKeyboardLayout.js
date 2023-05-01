eb8774f6af5d164745276507b3269e886211fe8b
Rodion <rodionserez@gmail.com>
2023-05-01 21:54:12 +0300

    feat: implement adding "shiftCaps" keys to keyboard

--- src/modules/getKeyboardLayout.js
+++ src/modules/getKeyboardLayout.js
@@ -4,15 +4,17 @@ export const getKeyboardLayout = (ENkeys, RUkeys) => {
 		const RUkey = RUkeys[i];
 		layoutString +=
 			`<div class="keyboard__key" id="${ENkey.code}">
-					<span class="rus hidden"> 
+					<span class="ru hidden"> 
 						<span class="caseDown">${RUkey.key}</span>
 						<span class="caseUp hidden">${RUkey.shift ? RUkey.shift : RUkey.key}</span>
 						<span class="caps hidden">${RUkey.caps ? RUkey.caps : RUkey.key}</span>
+						<span class="shiftCaps hidden">${RUkey.shiftCaps ? RUkey.shiftCaps : RUkey.key}</span>
 					</span>
 					<span class="en"> 
 						<span class="caseDown">${ENkey.key}</span>
 						<span class="caseUp hidden">${ENkey.shift ? ENkey.shift : ENkey.key}</span>
 						<span class="caps hidden">${ENkey.caps ? ENkey.caps : ENkey.key}</span>
+						<span class="shiftCaps hidden">${ENkey.shiftCaps ? ENkey.shiftCaps : ENkey.key}</span>
 					</span>
 			</div>`;
 		// if(key.shift) layoutString += `<div class="keyboard__key upperCase" id="${key.shift}">${key.shift}</div>`;
